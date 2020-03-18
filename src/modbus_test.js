ddc8e18170281188c605dedfb8061af5caa3c92f
Nicholas Ciarelli <nicholas.ciarelli@leverx.com>
2020-03-18 12:32:09 -0400

    Added reading of some coils and registers to test communication with the OpenPLC server.

--- src/modbus_test.js
+++ src/modbus_test.js
@@ -13,3 +13,24 @@ const options = {
 'host' : host,
 'port' : port
 }
+
+socket.on('connect', function () {
+ 
+    // make some calls
+    
+    
+    client.readCoils(0, 13).then(function (resp) {
+     
+        // resp will look like { response : [TCP|RTU]Response, request: [TCP|RTU]Request }
+        // the data will be located in resp.response.body.coils: <Array>, resp.response.body.payload: <Buffer>
+        console.log(`The following are the PLC output coils: \n${resp}`);
+     
+    }, console.error);
+
+    client.readHoldingRegisters(0, 10).then(function (resp) {
+        console.log(`The following are the PLC holding registers: \n${resp}`);
+    }, console.error);
+     
+});
+
+socket.connect(options);
