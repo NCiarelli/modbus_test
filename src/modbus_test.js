c6cd23ff2166f4575f48e00a79bddab4bb38d696
Nicholas Ciarelli <nicholas.ciarelli@leverx.com>
2020-03-20 10:49:12 -0400

    Fix code for jsmodbus using code from examples.

--- src/modbus_test.js
+++ src/modbus_test.js
@@ -1,62 +1,65 @@
+'use strict'
+
 // JSMODBUS SECTION
 
-// // OpenPLC Modbus server address
-// const host = "localhost";
-// const port = "502";
+// OpenPLC Modbus server address
+const host = "localhost";
+const port = "502";
 
-// // What is unitId here for?
-// const unitId = 1;
+// What is unitId here for?
+const unitId = 1;
 
-// const Modbus = require('jsmodbus');
-// const net = require('net');
-// const socket = new net.Socket();
-// const client = new Modbus.client.TCP(socket);
-// const options = {
-// 'host' : host,
-// 'port' : port
-// }
+const modbus = require('jsmodbus')
+const net = require('net')
+const socket = new net.Socket()
+const options = {
+  'host': host,
+  'port': port
+}
+const client = new modbus.client.TCP(socket)
 
+socket.on('connect', function () {
+  client.readCoils(0, 8)
+    .then(function (resp) {
+      console.log(resp.response._body.valuesAsArray)
+      socket.end()
+    }).catch(function () {
+      console.error(arguments)
+      socket.end()
+    })
+    client.readHoldingRegisters(0, 10)
+    .then(function (resp) {
+      console.log(resp.response._body.valuesAsArray)
+      socket.end()
+    }).catch(function () {
+      console.error(require('util').inspect(arguments, {
+        depth: null
+      }))
+      socket.end()
+    })
+})
 
-
-// socket.on('connect', function () {
- 
-//     // make some calls
-    
-    
-//     client.readCoils(0, 13).then(function (resp) {
-     
-//         // resp will look like { response : [TCP|RTU]Response, request: [TCP|RTU]Request }
-//         // the data will be located in resp.response.body.coils: <Array>, resp.response.body.payload: <Buffer>
-//         console.log(`The following are the PLC output coils: \n${resp}`);
-     
-//     }, console.error);
-
-//     // client.readHoldingRegisters(1024, 10).then(function (resp) {
-//     //     console.log(`The following are the PLC holding registers: \n${resp}`);
-//     // }, console.error);
-     
-// });
-
-// socket.connect(options);
+socket.on('error', console.error)
+socket.connect(options)
 
 
 // MODBUS-SERIAL SECTION
 
-// create an empty modbus client
-var ModbusRTU = require("modbus-serial");
-var client = new ModbusRTU();
+// // create an empty modbus client
+// var ModbusRTU = require("modbus-serial");
+// var client = new ModbusRTU();
  
-// open connection to a tcp line
-client.connectTCP("127.0.0.1", { port: 502 });
-client.setID(1);
+// // open connection to a tcp line
+// client.connectTCP(host, { port: 502 });
+// client.setID(1);
  
-// read the values of 10 registers starting at address 0
-// on device number 1. and log the values to the console.
-setInterval(function() {
-    client.readHoldingRegisters(1024, 10, function(err, data) {
-        console.log(data.data);
-    });
-    client.readCoils(0, 10, function(err, data) {
-        console.log(data.data);
-    });
-}, 1000);
+// // read the values of 10 registers starting at address 0
+// // on device number 1. and log the values to the console.
+// setInterval(function() {
+//     client.readHoldingRegisters(1024, 10, function(err, data) {
+//         console.log(data.data);
+//     });
+//     client.readCoils(0, 10, function(err, data) {
+//         console.log(data.data);
+//     });
+// }, 1000);
